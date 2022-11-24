2d9658fc389a3f5837216a7def57d24ff140a6df
Alysson Menezes <almenezes912@gmail.com>
2022-11-24 16:02:41 -0300

    finalizando login action

---
+++ SwiftAquiteturas/SwiftAquiteturas/Controllers/RegisterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegisterViewController.swift
+//  SwiftAquiteturas
+//
+//  Created by ALYSSON DODO on 24/11/22.
+//
+import UIKit
+import Foundation
+
+class RegisterViewController : UIViewController {
+    
+    @IBOutlet weak var registerLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func tappedRegisterButton(_ sender: Any) {
+       
+       
+    }
+    
+    
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+    }
+}
